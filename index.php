6adb8fe39a7f5230a3eaa7274c613421e62587e9
Pontus Björkdahl <pontus_pb95@hotmail.com>
2022-10-31 12:11:13 +0100

    Header

---
+++ index.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/small-desktops.css">
+    <link rel="stylesheet" href="css/tablets.css">
+    <link rel="stylesheet" href="css/mobile.css">
+    <title>Talex Webbshop</title>
+</head>
+<body>
+    <header>
+        <img id="hamburger-icon" src="img/radix-icons_hamburger-menu.svg" alt="">
+        <img id="logo" src="img/talex_icon.png" alt="">
+        <div class="navbar">
+            <div class="dropdown">
+                <button class="dropbtn">
+                    Start <i class="fa fa-caret-down"></i>
+                </button>
+                <div class="dropdown-content">
+                    <a href="">E-handelspaket</a>
+                    <a href="">Betalningslösningar</a>
+                    <a href="">Affärssystem</a>
+                    <a href="">Dropshipping</a>
+                </div>
+            </div>
+            <div class="dropdown">
+                <button class="dropbtn">
+                    Design <i class="fa fa-caret-down"></i>
+                </button>
+                <!-- <div class="dropdown-content">
+                    <a href="">E-handelspaket</a>
+                    <a href="">Betalningslösningar</a>
+                    <a href="">Affärssystem</a>
+                    <a href="">Dropshipping</a>
+                </div> -->
+            </div>
+            <div class="dropdown">
+                <button class="dropbtn">
+                    Moduler <i class="fa fa-caret-down"></i>
+                </button>
+                <!-- <div class="dropdown-content">
+                    <a href="">E-handelspaket</a>
+                    <a href="">Betalningslösningar</a>
+                    <a href="">Affärssystem</a>
+                    <a href="">Dropshipping</a>
+                </div> -->
+            </div>
+            <div class="dropdown">
+                <button class="dropbtn">
+                    Synlighet <i class="fa fa-caret-down"></i>
+                </button>
+                <!-- <div class="dropdown-content">
+                    <a href="">E-handelspaket</a>
+                    <a href="">Betalningslösningar</a>
+                    <a href="">Affärssystem</a>
+                    <a href="">Dropshipping</a>
+                </div> -->
+            </div>
+        </div>
+        <div id="header-action-img">
+            <img src="img/akar-icons_person.svg" alt="" id="login-img">
+            <img src="img/shopping-cart.svg" alt="" id="cart-img">
+        </div>
+    </header>
+    <div class="background-img-content">
+        <p>Välkommen till webbshoppen <br>
+        Här hittar du tilläggstjänster och moduler
+        </p>
+        <img src="img/background-logo.png" alt="" id="background-logo">
+        <p id="talex-text">TALEX</p>
+
+    </div>
+</body>
+</html>
